012105734bf204f6eaee06f9aa573ecbf89285f9
= <=>
2024-01-28 21:34:17 +0100

    Segunda versión del juego, esta vez con Bootstrap

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 function playGame(playerChoice) {
-    const choices = ['piedra', 'papel', 'tijera'];
-    const computerChoice = choices[Math.floor(Math.random() * 3)];
-    
-    let result = '';
+  const choices = ['piedra', 'papel', 'tijera'];
+  const computerChoice = choices[Math.floor(Math.random() * 3)];
   
-    if (playerChoice === computerChoice) {
-      result = 'Empate';
-    } else if (
-      (playerChoice === 'piedra' && computerChoice === 'tijera') ||
-      (playerChoice === 'papel' && computerChoice === 'piedra') ||
-      (playerChoice === 'tijera' && computerChoice === 'papel')
-    ) {
-      result = '¡Ganaste!';
-    } else {
-      result = '¡Perdiste!';
-    }
-  
-    document.getElementById('result').innerText = `Elegiste ${playerChoice}. La computadora eligió ${computerChoice}. ${result}`;
+  let result = '';
+
+  if (playerChoice === computerChoice) {
+    result = 'Empate';
+  } else if (
+    (playerChoice === 'piedra' && computerChoice === 'tijera') ||
+    (playerChoice === 'papel' && computerChoice === 'piedra') ||
+    (playerChoice === 'tijera' && computerChoice === 'papel')
+  ) {
+    result = '¡Ganaste!';
+  } else {
+    result = '¡Perdiste!';
   }
-  
+
+  document.getElementById('result').innerHTML = `<p>Elegiste ${playerChoice}. La computadora eligió ${computerChoice}. ${result}</p>`;
+}
